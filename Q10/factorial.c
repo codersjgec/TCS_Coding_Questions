1355effe0a485b90bc5273f1a3a2aee00b8594cf
Somnath Rakshit <somnath52@gmail.com>
2017-11-08 12:34:07 +0530

    Rewrote Fatorial in Q10

--- Q10/factorial.c
+++ Q10/factorial.c
@@ -3,16 +3,17 @@
 
 int factorial(int n)
 {
-    int ans=1;
-    for(int i=2;i<=n;i++)
-        ans = ans*i;
+    int i, ans = 1;
+    for (i = 2; i <= n; i++)
+        ans = ans * i;
     return ans;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     int a, ans;
     a = atoi(argv[1]);
     ans = factorial(a);
     printf("%d\n", ans);
     return 0;
-}   
+}
